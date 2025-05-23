3f1cb63c3475765574d42977a0fdfe279bd0568a
Rahul Kushwaha <rahulkcoding2023@gmail.com>
2025-05-24 00:04:39 +0530

    Time: 34 ms (5.26%), Space: 55.5 MB (26.32%) - LeetHub

--- 3307-find-the-maximum-sum-of-node-values/3307-find-the-maximum-sum-of-node-values.java
+++ 3307-find-the-maximum-sum-of-node-values/3307-find-the-maximum-sum-of-node-values.java
@@ -1,3 +1,39 @@
+//Approach-2(Greedy  + Sorting)
+//T.C : O(nlogn)
+//S.C : O(n)
+class Solution {
+    public long maximumValueSum(int[] nums, int k, int[][] edges) {
+        List<Integer> fayda = new ArrayList<>();
+        long normalSum = 0;
+
+        for (int num : nums) {
+            fayda.add((num ^ k) - num);
+            normalSum += (long) num;
+        }
+
+        Collections.sort(fayda, Collections.reverseOrder());
+
+        for (int i = 0; i < fayda.size() - 1; i += 2) {
+            long pairSum = fayda.get(i) + fayda.get(i + 1);
+
+            if (pairSum > 0) {
+                normalSum += pairSum;
+            }
+        }
+        return normalSum;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+/*
 //Approach-1 (Greedily picking nodes to xor)
 //T.C : O(n)
 //S.C : O(1)
@@ -24,3 +60,4 @@ class Solution {
         return sum - minNukasan;
     }
 }
+*/
