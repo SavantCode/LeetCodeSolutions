963cfafb91777aa643474ddf86b61890d1ebce50
Rahul Kushwaha <rahulkcoding2023@gmail.com>
2025-05-24 00:04:02 +0530

    Time: 2 ms (76.32%), Space: 54.5 MB (69.74%) - LeetHub

---
+++ 3307-find-the-maximum-sum-of-node-values/3307-find-the-maximum-sum-of-node-values.java
@@ -0,0 +1,26 @@
+//Approach-1 (Greedily picking nodes to xor)
+//T.C : O(n)
+//S.C : O(1)
+class Solution {
+    public long maximumValueSum(int[] nums, int k, int[][] edges) {
+        long sum = 0;
+        int count = 0;
+        int minNukasan = Integer.MAX_VALUE;
+
+        for (int num : nums) {
+            if ((num ^ k) > num) {
+                count++;
+                sum += (num ^ k);
+            } else {
+                sum += num;
+            }
+
+            minNukasan = Math.min(minNukasan, Math.abs(num - (num ^ k)));
+        }
+
+        if (count % 2 == 0)
+            return sum;
+
+        return sum - minNukasan;
+    }
+}
