589014e40dca75c3546c1cd20e672e776d0228f2
Rahul Kushwaha <rahulkcoding2023@gmail.com>
2025-06-01 23:13:00 +0530

    Time: 1 ms (25.29%), Space: 45.6 MB (6.61%) - LeetHub

--- 0109-convert-sorted-list-to-binary-search-tree/0109-convert-sorted-list-to-binary-search-tree.java
+++ 0109-convert-sorted-list-to-binary-search-tree/0109-convert-sorted-list-to-binary-search-tree.java
@@ -1,5 +1,43 @@
-import java.util.*;
+public class Solution {
+    private TreeNode helper(int[] nums, int lo, int hi) {
+        if (lo > hi) return null;
 
+        int mid = (lo + hi) / 2;
+        TreeNode root = new TreeNode(nums[mid]);
+        root.left = helper(nums, lo, mid - 1);
+        root.right = helper(nums, mid + 1, hi);
+
+        return root;
+    }
+
+    public TreeNode sortedListToBST(ListNode head) {
+        //Calculate lenth of LL
+        ListNode temp = head;
+        int n = 0;
+        while(temp != null){
+            n++;
+            temp = temp.next;
+        }
+        //filling the nums array
+        temp = head;
+        int[] nums = new int[n];
+        int idx = 0;
+        while(temp != null){
+            nums[idx++] = temp.val;
+            temp = temp.next;
+        }
+        return helper(nums,0,n-1);
+    }
+}
+
+// | Metric    | Complexity |
+// | --------- | ---------- |
+// | **Time**  | `O(n)`     |
+// | **Space** | `O(n)`     |
+
+
+
+/*
 public class Solution {
 
     public TreeNode sortedListToBST(ListNode head) {
@@ -33,3 +71,4 @@ public class Solution {
         return values;
     }
 }
+*/
