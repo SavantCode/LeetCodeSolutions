ad820f71a4f33e1d49428234ef962d5bca85c80b
Rahul Kushwaha <rahulkcoding2023@gmail.com>
2025-06-19 12:09:24 +0530

    Time: 23 ms (81.91%), Space: 59.4 MB (75.38%) - LeetHub

--- 3241-divide-array-into-arrays-with-max-difference/3241-divide-array-into-arrays-with-max-difference.java
+++ 3241-divide-array-into-arrays-with-max-difference/3241-divide-array-into-arrays-with-max-difference.java
@@ -1,3 +1,45 @@
+class Solution {
+    public int[][] divideArray(int[] nums, int k) {
+        Arrays.sort(nums); // Sort the array to group close values --> O(n log n)
+        int n = nums.length;
+
+        // If we can't divide into groups of 3, return empty array
+        if (n % 3 != 0) {
+            return new int[0][];
+        }
+
+        int[][] result = new int[n / 3][3]; // Prepare result array
+        int index = 0;
+
+        for (int i = 0; i < n; i += 3) { // O(n)
+            int a = nums[i];
+            int b = nums[i + 1];
+            int c = nums[i + 2];
+
+            // Since sorted, max - min = c - a
+            if (c - a > k) {
+                return new int[0][]; // prints: [] not [ [] ]
+            }
+
+            result[index][0] = a;
+            result[index][1] = b;
+            result[index][2] = c;
+            index++;
+        }
+
+        return result;// We're returning a new 2D array--> SC: O(n)
+    }
+}
+
+// | Resource | Complexity |
+// | -------- | ---------- |
+// | Time     | O(n log n) |
+// | Space    | O(n)       |
+
+
+
+/*
+
 //T.C : O(nlogn)
 //S.C : O(1)
 class Solution {
@@ -20,3 +62,6 @@ class Solution {
         return result;
     }
 }
+
+
+*/
