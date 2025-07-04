da9fe5c5e5ee9dccbd4c546d25dc1fccb9dfd110
Rahul Kushwaha <rahulkcoding2023@gmail.com>
2025-07-05 00:25:27 +0530

    Time: 0 ms (100%), Space: 45.1 MB (66.09%) - LeetHub

--- 0801-is-graph-bipartite/0801-is-graph-bipartite.java
+++ 0801-is-graph-bipartite/0801-is-graph-bipartite.java
@@ -1,39 +1,45 @@
+// 1 red color, 0 blue color
+
 class Solution {
     public boolean isBipartite(int[][] graph) {
         int V = graph.length;
         int[] color = new int[V];
-        Arrays.fill(color, -1); // no node colored in the start
-
-        // red = 1
-        // green = 0
+        Arrays.fill(color, -1);
 
         for (int i = 0; i < V; i++) {
             if (color[i] == -1) {
-                if (!dfs(graph, i, color, 1))
+                if (!checkBipartiteDFS(graph, i, color, 1)) {
                     return false;
+                }
             }
         }
 
         return true;
     }
 
-    private boolean dfs(int[][] graph, int curr, int[] color, int currColor) {
-        color[curr] = currColor; // color kardiya curr node ko
+    private boolean checkBipartiteDFS(int[][] graph, int node, int[] color, int currColor) {
+        color[node] = currColor;
 
-        // ab jaate hain adjacent nodes par
-        for (int v : graph[curr]) {
-
-            if (color[v] == color[curr])
+        for (int neighbor : graph[node]) {
+            if (color[neighbor] == color[node]) {
                 return false;
+            }
 
-            if (color[v] == -1) { // never colored (never visited)
-                int colorOfV = 1 - currColor;
-
-                if (!dfs(graph, v, color, colorOfV))
+            if (color[neighbor] == -1) {
+                if (!checkBipartiteDFS(graph, neighbor, color, 1 - currColor)) {
                     return false;
+                }
             }
         }
 
         return true;
     }
 }
+
+// | Complexity | Value    |
+// | ---------- | -------- |
+// | **Time**   | O(V + E) |
+// | **Space**  | O(V)     |
+
+// V is the number of vertices (nodes) = graph.length
+// E is the total number of edges (sum of all neighbor lists' sizes)
