fafa9565260d9c27dd0ef96aaaa05d133bb40ffe
Rahul Kushwaha <rahulkcoding2023@gmail.com>
2025-07-09 22:58:28 +0530

    Time: 2 ms (76.11%), Space: 61.4 MB (76.11%) - LeetHub

---
+++ 3743-reschedule-meetings-for-maximum-free-time-i/3743-reschedule-meetings-for-maximum-free-time-i.java
@@ -0,0 +1,47 @@
+/*         Scroll below to see JAVA code also        */
+/*
+    MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=JPWBTUyGCnM
+    Company Tags                : will update soon
+    Leetcode Link               : https://leetcode.com/problems/reschedule-meetings-for-maximum-free-time-i
+*/
+
+/********************************************************************** JAVA **********************************************************************/
+//Approach  (Sliding Window)
+//T.C : O(n)
+//S.C : O(k)
+class Solution {
+    public int maxFreeTime(int eventTime, int k, int[] startTime, int[] endTime) {
+        int n = startTime.length;
+        int[] freeArray = new int[n + 1];
+        
+        // First gap: from time 0 to startTime <sup> </sup>
+        freeArray[0] = startTime[0];
+        
+        // Gaps between consecutive events
+        for (int i = 1; i < n; i++) {
+            freeArray[i] = startTime[i] - endTime[i - 1];
+        }
+        
+        // Last gap: from endTime[n-1] to eventTime
+        freeArray[n] = eventTime - endTime[n - 1];
+        
+        int maxSum = 0;
+        int currSum = 0;
+        int left = 0;
+        
+        // Sliding window to find maximum sum of (k+1) gaps
+        for (int right = 0; right < freeArray.length; right++) {
+            currSum += freeArray[right];
+            
+            // Window size should be at most (k+1)
+            while (right - left + 1 > k + 1) {
+                currSum -= freeArray[left];
+                left++;
+            }
+            
+            maxSum = Math.max(maxSum, currSum);
+        }
+        
+        return maxSum;
+    }
+}
