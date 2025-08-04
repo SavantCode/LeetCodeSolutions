4394d0cb258de731b50dc5b6a879f74df007906a
Rahul Kushwaha <rahulkcoding2023@gmail.com>
2025-08-05 01:07:51 +0530

    Time: 50 ms (59.78%), Space: 55.3 MB (40.84%) - LeetHub

--- 0940-fruit-into-baskets/0940-fruit-into-baskets.java
+++ 0940-fruit-into-baskets/0940-fruit-into-baskets.java
@@ -1,31 +1,33 @@
 class Solution {
     public int totalFruit(int[] fruits) {
-        int n = fruits.length;
-        HashMap<Integer, Integer> fruitMap = new HashMap<>();
+        Map<Integer, Integer> basket = new HashMap<>();
+        int i = 0, max = 0;
 
-        int i = 0; // Left pointer
-        int j = 0; // Right pointer
-        int count = 0; // Maximum number of fruits collected
+        for (int j = 0; j < fruits.length; j++) {
+            int fruit = fruits[j];
+            basket.put(fruit, basket.getOrDefault(fruit, 0) + 1);
 
-        while (j < n) {
-            // Add the fruit at the right pointer to the map
-            fruitMap.put(fruits[j], fruitMap.getOrDefault(fruits[j], 0) + 1);
-
-            // While more than 2 types of fruits are in the map, move the left pointer
-            if (fruitMap.size() > 2) {
-                fruitMap.put(fruits[i], fruitMap.get(fruits[i]) - 1);
-                if (fruitMap.get(fruits[i]) == 0) {
-                    fruitMap.remove(fruits[i]);
+            // Shrink the window from the left if we have more than 2 types
+            // actually we are shrinking the window using while loop
+            while (basket.size() > 2) {
+                int leftFruit = fruits[i];
+                basket.put(leftFruit, basket.get(leftFruit) - 1);
+                if (basket.get(leftFruit) == 0) {
+                    basket.remove(leftFruit);
                 }
-                i++; // Move the left pointer to the right
+                i++;
             }
 
-            // Calculate the maximum count of fruits collected
-            count = Math.max(count, j - i + 1);
-
-            // Move the right pointer to the right
-            j++;
+            // Update max length of valid window
+            max = Math.max(max, j - i + 1);
         }
-        return count;
+
+        return max;
     }
 }
+
+/*
+
+//T.C : O(n)
+//S.C : O(n)
+*/
