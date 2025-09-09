894bc50078d5976766719c363db90b65e1a879f6
Rahul Kushwaha <rahulkcoding2023@gmail.com>
2025-09-09 10:58:41 +0530

    Time: 0 ms (100%), Space: 43.9 MB (66.84%) - LeetHub

--- 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.java
+++ 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.java
@@ -6,23 +6,53 @@
  *     ListNode(int x) { val = x; }
  * }
  */
-//T.C : O(n)
-//S.C : O(1)
-class Solution {
-    public void deleteNode(ListNode node) {
-        ListNode prev = null;
 
-        while (node != null && node.next != null) {
-            node.val = node.next.val;
-            prev = node;
-            node = node.next;
-        }
-        
-        if (node != null) {
-            if (prev != null)
-                prev.next = null;
-            else
-                node = null;
-        }
+class Solution {
+    /**
+     * Deletes a node (except the tail) in a singly linked list,
+     * given only access to that node.
+     *
+     * Time Complexity: O(1)
+     * Space Complexity: O(1)
+     *
+     * @param node The node to be "deleted"
+     */
+    public void deleteNode(ListNode node) {
+        // Copy the next node's value into the current node
+        node.val = node.next.val;
+
+        // Skip the next node to remove it from the list
+        node.next = node.next.next;
     }
 }
+
+
+
+// /**
+//  * Definition for singly-linked list.
+//  * public class ListNode {
+//  *     int val;
+//  *     ListNode next;
+//  *     ListNode(int x) { val = x; }
+//  * }
+//  */
+// //T.C : O(n)
+// //S.C : O(1)
+// class Solution {
+//     public void deleteNode(ListNode node) {
+//         ListNode prev = null;
+
+//         while (node != null && node.next != null) {
+//             node.val = node.next.val;
+//             prev = node;
+//             node = node.next;
+//         }
+        
+//         if (node != null) {
+//             if (prev != null)
+//                 prev.next = null;
+//             else
+//                 node = null;
+//         }
+//     }
+// }
