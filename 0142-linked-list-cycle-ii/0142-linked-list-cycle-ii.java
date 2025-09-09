79bb7c92397d642e64375879feb3abd3d3193a1e
Rahul Kushwaha <rahulkcoding2023@gmail.com>
2025-09-09 19:07:42 +0530

    Time: 0 ms (100%), Space: 44.6 MB (69.32%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
@@ -9,22 +9,61 @@
  *     }
  * }
  */
-public class Solution {
-    public ListNode detectCycle(ListNode head) {
-        Set<ListNode> visited = new HashSet<>();
-        ListNode current = head;
 
-        while (current != null) {
-            if (visited.contains(current)) {
-                // Cycle detected; current is start of cycle
-                return current;
-            }
-            visited.add(current);
-            current = current.next;
+
+//T.C : O(n)
+//S.C : O(1)
+public class Solution {
+    
+    public ListNode detectCycle(ListNode head){
+        if(head == null ||(head.next == null)) return null;
+
+        ListNode fast = head;
+        ListNode slow = head;
+
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+
+
+            if(slow == fast) break;
         }
 
-        // No cycle found
-        return null;
+        if(slow !=fast ) return null;
+
+        ListNode p = head;
+        
+        while(p != slow){
+            p = p.next;
+            slow = slow.next;
+        }
+        return slow; // or you can return 'p' also since 'p' and 'slow' both are same at this point
     }
 
 }
+
+
+
+
+
+
+//brute force
+// public class Solution {
+//     public ListNode detectCycle(ListNode head) {
+//         Set<ListNode> visited = new HashSet<>();
+//         ListNode current = head;
+
+//         while (current != null) {
+//             if (visited.contains(current)) {
+//                 // Cycle detected; current is start of cycle
+//                 return current;
+//             }
+//             visited.add(current);
+//             current = current.next;
+//         }
+
+//         // No cycle found
+//         return null;
+//     }
+
+// }
