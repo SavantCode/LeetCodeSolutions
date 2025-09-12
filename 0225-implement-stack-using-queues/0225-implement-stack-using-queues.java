eb24defee7216a79005f0399d10b371f6dc6ced1
Rahul Kushwaha <rahulkcoding2023@gmail.com>
2025-09-12 18:23:14 +0530

    Time: 0 ms (100%), Space: 41.2 MB (92.76%) - LeetHub

--- 0225-implement-stack-using-queues/0225-implement-stack-using-queues.java
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.java
@@ -1,3 +1,8 @@
+//approach 2: efficient
+
+// TC: O(1)
+// SC: push--> O(n), pop--> O(1), peek--> O(1)
+
 import java.util.LinkedList;
 import java.util.Queue;
 
@@ -29,3 +34,53 @@ class MyStack {
         return q.isEmpty();
     }
 }
+
+
+
+
+
+
+//approach 1
+/* 
+import java.util.LinkedList;
+import java.util.Queue;
+
+class MyStack {
+    Queue<Integer> q = new LinkedList<>();
+
+    public MyStack() {
+    }
+
+    public void push(int x) {
+        q.add(x);
+    }
+
+    public int pop() {
+        int size = q.size();
+        for (int i = 0; i < size - 1; i++) {
+            q.add(q.remove());
+        }
+
+        return q.remove();  // Remove the "top" element
+    }
+
+    public int top() {
+        int size = q.size();
+        for (int i = 0; i < size - 1; i++) {
+            q.add(q.remove());
+        }
+
+        int top = q.peek();  // Peek the front element
+        q.add(q.remove());   // Put it back to maintain order
+
+        return top;
+    }
+
+    public boolean empty() {
+        return q.isEmpty();
+    }
+}
+
+TC : O(1)
+SC : push()--> O(1), pop()--> O(n), pek()--> O(n)
+*/
