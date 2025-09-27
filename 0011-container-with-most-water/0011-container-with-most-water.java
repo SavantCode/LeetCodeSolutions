e4cad39565da6020c6ad204d9eeea8d202a7da2e
Rahul Kushwaha <rahulkcoding2023@gmail.com>
2025-09-27 19:19:06 +0530

    Time: 5 ms (77.15%), Space: 58 MB (44.43%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.java
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -1,17 +1,16 @@
+//O(n), avoiding TLE.
 class Solution {
     public int maxArea(int[] height) {
-        int maxArea = 0;
         int left = 0;
         int right = height.length - 1;
+        int maxArea = 0;
 
         while (left < right) {
-            int minHeight = Math.min(height[left], height[right]);
-            int width = right - left;
-            int area = minHeight * width;
+            int currHeight = Math.min(height[left], height[right]);
+            int currWidth = right - left;
+            int currArea = currHeight * currWidth;
+            maxArea = Math.max(maxArea, currArea);
 
-            maxArea = Math.max(maxArea, area);
-
-            // Move the pointer pointing to the shorter line
             if (height[left] < height[right]) {
                 left++;
             } else {
@@ -22,3 +21,27 @@ class Solution {
         return maxArea;
     }
 }
+
+
+
+
+//-------------------Getting  TLE  for this -- O(n²)
+// import java.util.Arrays;
+
+// class Solution {
+//     public int maxArea(int[] height) {
+        
+//         int area = Integer.MIN_VALUE;
+//         int n = height.length;
+//         for(int i = 0; i < n; i++){
+//             for(int j = 0; j < n; j++){
+//                 int currHeight =Math.min(height[i], height[j]);
+
+//                 int currWidth = Math.abs(j - i);
+//                 int currArea = currHeight * currWidth;
+//                 area = Math.max(currArea, area);
+//             }
+//         }
+//         return area;
+//     }
+// }
