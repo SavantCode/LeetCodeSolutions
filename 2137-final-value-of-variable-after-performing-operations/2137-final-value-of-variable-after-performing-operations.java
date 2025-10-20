ea64c04f176ef3552ee6f755313f3729c54f66ab
Rahul Kushwaha <rahulkcoding2023@gmail.com>
2025-10-21 00:45:44 +0530

    Time: 1 ms (74.53%), Space: 42.6 MB (77.82%) - LeetHub

--- 2137-final-value-of-variable-after-performing-operations/2137-final-value-of-variable-after-performing-operations.java
+++ 2137-final-value-of-variable-after-performing-operations/2137-final-value-of-variable-after-performing-operations.java
@@ -1,15 +1,28 @@
+// Time Complexity: O(n)
+// Space Complexity: O(1)
 class Solution {
     public int finalValueAfterOperations(String[] operations) {
-        int X = 0; // Initial value of X is 0
-
-        for (String operation : operations) {
-            if (operation.equals("++X") || operation.equals("X++")) {
+        int n = operations.length;
+        int X = 0;
+        for(int i = 0; i < n ; i++) {
+            if(operations[i].equals("X++") || operations[i].equals("++X")) {
                 X++;
             } else {
                 X--;
             }
         }
-
         return X;
     }
 }
+
+// my NOTES
+/*
+In Java:
+
+== checks if two references point to the same object.
+
+.equals() checks if the content of the strings is the same.
+
+For string comparison, always use .equals() unless you're checking whether two references are the exact same object (which is rare).
+
+*/
