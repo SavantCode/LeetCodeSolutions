1d68d7c0cc3c7821f629ea3bab1aa2d003e9aa80
Rahul Kushwaha <rahulkcoding2023@gmail.com>
2025-10-25 23:47:01 +0530

    Time: 2 ms (17.17%), Space: 41 MB (9.09%) - LeetHub

--- 1817-calculate-money-in-leetcode-bank/1817-calculate-money-in-leetcode-bank.java
+++ 1817-calculate-money-in-leetcode-bank/1817-calculate-money-in-leetcode-bank.java
@@ -1,3 +1,29 @@
+//Approach-1 (Simple simulation)
+//T.C : O(1) - see the video above to know why
+//S.C : O(1)
+class Solution {
+    public int totalMoney(int n) {
+        int result = 0;
+        int mondayMoney = 1;
+
+        while (n > 0) {
+            int money = mondayMoney;
+            for (int day = 1; day <= Math.min(n, 7); day++) {
+                result += money;
+                money++;
+            }
+
+            n -= 7;
+            mondayMoney++;
+        }
+
+        return result;
+    }
+}
+
+
+
+/* My Appraoch
 // Complexity
 // Time: O(n) — single loop through all days.
 // Space: O(1) — only uses constant variables.
@@ -27,4 +53,57 @@ class Solution {
         return leetcodeBank;
 
     }
+}*/
+
+
+/*
+
+//Approach-2 (Using AP and a for loop)
+//T.C : O(1) - see the video above to know why
+//S.C : O(1)
+class Solution {
+    public int totalMoney(int n) {
+        int terms = n / 7; // Weeks
+
+        int first = 28;
+        int last = 28 + (terms - 1) * 7; // AP formula for n-th term
+
+        int result = terms * (first + last) / 2; // Sum of nth terms in an AP
+
+        // Final week remaining days = n % 7
+        int startMoney = 1 + terms;
+
+        for (int day = 1; day <= (n % 7); day++) {
+            result += startMoney;
+            startMoney++;
+        }
+
+        return result;
+    }
 }
+
+
+
+//Approach-3 (Using AP completely)
+//T.C : O(1) - see the video above to know why
+//S.C : O(1)
+class Solution {
+    public int totalMoney(int n) {
+        int terms = n / 7; // Weeks
+
+        int first = 28;
+        int last = 28 + (terms - 1) * 7; // AP formula for n-th term
+
+        int result = terms * (first + last) / 2; // Sum of nth terms in an AP
+
+        // Remaining days (use AP formula)
+        int remain = n % 7;
+        int firstTerm = 1 + terms;
+        int lastTerm = firstTerm + remain - 1;
+        result += remain * (firstTerm + lastTerm) / 2;
+
+        return result;
+    }
+}
+
+ */
