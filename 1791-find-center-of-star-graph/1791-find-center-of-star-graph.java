1c95dbb868f875e6bb907f12109f032a89af5279
Rahul Kushwaha <rahulkcoding2023@gmail.com>
2026-09-04 14:34:31 +0530

    Time: 36 ms (7%), Space: 122.3 MB (17.79%) - LeetHub

--- 1791-find-center-of-star-graph/1791-find-center-of-star-graph.java
+++ 1791-find-center-of-star-graph/1791-find-center-of-star-graph.java
@@ -1,62 +1,24 @@
+//Approach-1 (Using map)
+//T.C : O(n)
+//S.C : O(n)
 class Solution {
     public int findCenter(int[][] edges) {
-
-        // MISTAKE 1:
-        // adj is initially empty.
-        // We need to create a list for every node before using adj.get(u).
-        int n = edges.length + 1;
-
-        List<List<Integer>> adj = new ArrayList<>();
-
-        // Initialize adjacency list
-        // Node numbers start from 1, so we use <= n.
-        for (int i = 0; i <= n; i++) {
-            adj.add(new ArrayList<>());
-        }
+        Map<Integer, Integer> degree = new HashMap<>();
 
         for (int[] edge : edges) {
             int u = edge[0];
             int v = edge[1];
 
-            // MISTAKE 2:
-            // You only had:
-            // adj.get(u).add(v);
-            //
-            // But this is an UNDIRECTED graph.
-            // So we need to add both directions.
-            adj.get(u).add(v);
-            adj.get(v).add(u);
+            degree.put(u, degree.getOrDefault(u, 0) + 1);
+            degree.put(v, degree.getOrDefault(v, 0) + 1);
         }
 
-        int max = Integer.MIN_VALUE;
-
-        // MISTAKE 3:
-        // n = adj.size() would work here because we created n + 1 lists,
-        // but node 0 is not an actual node.
-        // So we iterate from 1 to n.
-        int center = -1;
-
-        for (int i = 1; i <= n; i++) {
-
-            int currLen = adj.get(i).size();
-
-            // MISTAKE 4:
-            // You were only storing the maximum degree:
-            // max = Math.max(currLen, max);
-            //
-            // But we need to RETURN THE NODE having maximum degree,
-            // not the degree itself.
-            if (currLen > max) {
-                max = currLen;
-                center = i;
+        for (Map.Entry<Integer, Integer> entry : degree.entrySet()) {
+            if (entry.getValue() == edges.length) {
+                return entry.getKey(); // star graph center node
             }
         }
 
-        // MISTAKE 5:
-        // return max;
-        // This would return the maximum degree.
-        //
-        // We need to return the node with maximum degree.
-        return center;
+        return -1;
     }
 }
