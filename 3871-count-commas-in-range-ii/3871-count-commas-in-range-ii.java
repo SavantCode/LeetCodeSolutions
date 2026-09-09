74e4fc9bf7ebd6fe90658a987c07880957226afa
Rahul Kushwaha <rahulkcoding2023@gmail.com>
2026-09-09 21:52:46 +0530

    Time: 1 ms (99.36%), Space: 42.7 MB (56.41%) - LeetHub

--- 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.java
+++ 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.java
@@ -1,36 +1,18 @@
+//T.C : O(log1000(n))
+//S.C : O(1)
 class Solution {
     public long countCommas(long n) {
-
-        if (n < 1_000) {
-            return 0;
+        long result = 0;
+        long lower  = 1000;
+        long commas = 1;
+        while (lower <= n) {
+            long upper = lower * 1000 - 1;
+            if (upper > n) upper = n;
+            long countNumbers = upper - lower + 1;
+            result += (countNumbers * commas);
+            lower *= 1000;
+            commas++;
         }
-
-        if (n < 1_000_000) {
-            return n - 999;
-        }
-
-        if (n < 1_000_000_000) {
-            return (1_000_000 - 1_000)
-                    + 2L * (n - 999_999);
-        }
-
-        if (n < 1_000_000_000_000L) {
-            return (1_000_000 - 1_000)
-                    + 2L * (1_000_000_000L - 1_000_000)
-                    + 3L * (n - 999_999_999L);
-        }
-
-        if (n < 1_000_000_000_000_000L) {
-            return (1_000_000 - 1_000)
-                    + 2L * (1_000_000_000L - 1_000_000)
-                    + 3L * (1_000_000_000_000L - 1_000_000_000L)
-                    + 4L * (n - 999_999_999_999L);
-        }
-
-        return (1_000_000 - 1_000)
-                + 2L * (1_000_000_000L - 1_000_000)
-                + 3L * (1_000_000_000_000L - 1_000_000_000L)
-                + 4L * (1_000_000_000_000_000L - 1_000_000_000_000L)
-                + 5L * (n - 999_999_999_999_999L);
+        return result;
     }
 }
