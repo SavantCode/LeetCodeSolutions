9c4c625db0b8b542512e8052b637f1a2f13de606
Rahul Kushwaha <rahulkcoding2023@gmail.com>
2026-09-11 15:56:50 +0530

    Time: 465 ms (5.02%), Space: 283.7 MB (5.05%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -1,28 +1,53 @@
-// 1D DP:
-// O(n × target) time, O(target) space
+//Approach-1 (Recursion + Memo)
+//T.C : O(n*x), where x = totalSum/2
+//S.C : O(n*x)
 class Solution {
-    public boolean canPartition(int[] nums) {
-        int totalSum = 0;
+    int[][] t = new int[201][20001];
 
-        for (int num : nums) {
-            totalSum += num;
+    boolean solve(List<Integer> nums, int i, int x) {
+        if (x == 0) {
+            return true;
         }
 
-        if (totalSum % 2 != 0) {
+        if (i >= nums.size()) {
             return false;
         }
 
-        int target = totalSum / 2;
-
-        boolean[] dp = new boolean[target + 1];
-        dp[0] = true;
-
-        for (int num : nums) {
-            for (int sum = target; sum >= num; sum--) {
-                dp[sum] = dp[sum] || dp[sum - num];
-            }
+        if (t[i][x] != -1) {
+            return t[i][x] == 1;
         }
 
-        return dp[target];
+        boolean take = false;
+        if (nums.get(i) <= x) {
+            take = solve(nums, i + 1, x - nums.get(i));
+        }
+
+        boolean not_take = solve(nums, i + 1, x);
+
+        t[i][x] = (take || not_take) ? 1 : 0;
+        return take || not_take;
+    }
+
+    public boolean canPartition(int[] numsArr) {
+        List<Integer> nums = new ArrayList<>();
+        for (int num : numsArr) nums.add(num);
+
+        int n = nums.size();
+        int S = 0;
+        for (int num : nums) {
+            S += num;
+        }
+
+        if (S % 2 != 0) {
+            return false;
+        }
+
+        for (int[] row : t) {
+            Arrays.fill(row, -1);
+        }
+
+        int x = S / 2;
+
+        return solve(nums, 0, x);
     }
 }
