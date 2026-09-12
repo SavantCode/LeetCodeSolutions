e87f71fed5f7273a38571195eaf98b9a54eedc75
Rahul Kushwaha <rahulkcoding2023@gmail.com>
2026-09-12 19:13:04 +0530

    Time: 2 ms (10.62%), Space: 47.1 MB (10.89%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,7 +1,7 @@
 class Solution {
 
-    public class Pair {
-
+    // Stores the row and column of a cell
+    class Pair {
         int i;
         int j;
 
@@ -16,27 +16,45 @@ class Solution {
         int rows = image.length;
         int cols = image[0].length;
 
+        // Queue is used for BFS traversal
         Queue<Pair> queue = new LinkedList<>();
 
+        // Store the color of the starting cell.
+        // We only want to change cells having this color.
         int originalColor = image[sr][sc];
 
-        int directions[][] = {
+        // 4 possible directions:
+        // right, left, down, up
+        int[][] directions = {
             {0, 1},
             {0, -1},
-            {1, 0}, 
+            {1, 0},
             {-1, 0}
-        }; // right, left, down, up
+        };
 
-        // Important: If the original color is already the target color,
-        // we can return immediately. Otherwise BFS can keep processing
-        // unnecessarily.
+        // If the starting color is already the target color,
+        // nothing needs to be changed.
+        // Also prevents unnecessary BFS processing.
         if (originalColor == color) {
             return image;
         }
 
+        // Change the starting cell to the new color.
+        // This also marks it as visited.
         image[sr][sc] = color;
 
-        solve(queue, sr, sc, image, directions, color, originalColor, rows, cols);
+        // Start BFS from the source cell
+        solve(
+            queue,
+            sr,
+            sc,
+            image,
+            directions,
+            color,
+            originalColor,
+            rows,
+            cols
+        );
 
         return image;
     }
@@ -53,34 +71,62 @@ class Solution {
         int cols
     ) {
 
-        // Mistake: queue is Queue<int[]>, but you were trying to add Pair.
-        // Also Pair(i, j) must use the 'new' keyword.
+        // Add the starting cell to the BFS queue
         queue.add(new Pair(i, j));
 
+        // Continue until there are no more cells to process
         while (!queue.isEmpty()) {
 
-            Pair top = queue.poll();
+            // Remove the front cell from the queue
+            Pair current = queue.poll();
 
-            int currRow = top.i;
-            int currCol = top.j;
+            int currRow = current.i;
+            int currCol = current.j;
 
+            // Try all 4 neighboring cells
             for (int[] dir : directions) {
 
                 int newRow = currRow + dir[0];
                 int newCol = currCol + dir[1];
 
-                // Mistake: isSafe() was not defined.
-                // We check that the new position is inside the matrix.
+                /*
+                 * A neighboring cell can be added to BFS only if:
+                 *
+                 * 1. It is inside the matrix
+                 * 2. Its color is the original color
+                 *
+                 * We don't need a separate visited array because
+                 * changing its color also marks it as visited.
+                 */
                 if (newRow >= 0 && newRow < rows &&
                     newCol >= 0 && newCol < cols &&
                     image[newRow][newCol] == originalColor) {
 
-                    // Mark as visited by changing its color.
+                    // Change color immediately.
+                    // This prevents the same cell from being
+                    // added to the queue multiple times.
                     image[newRow][newCol] = color;
 
+                    // Add this cell to BFS for further exploration
                     queue.add(new Pair(newRow, newCol));
                 }
             }
         }
     }
 }
+
+// Start cell
+//     ↓
+// Put in queue
+//     ↓
+// Take cell from queue
+//     ↓
+// Check 4 neighbours
+//     ↓
+// Is neighbour inside grid?
+//     ↓
+// Does neighbour have original color?
+//     ↓
+// Change its color + put in queue
+//     ↓
+// Repeat
