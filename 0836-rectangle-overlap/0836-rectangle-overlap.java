b7232bbee8eaeeff587336a4fc2005e26b98ac41
Rahul Kushwaha <rahulkcoding2023@gmail.com>
2026-09-14 23:30:54 +0530

    Time: 0 ms (100%), Space: 42.9 MB (17.64%) - LeetHub

--- 0836-rectangle-overlap/0836-rectangle-overlap.java
+++ 0836-rectangle-overlap/0836-rectangle-overlap.java
@@ -1,11 +1,15 @@
 class Solution {
     public boolean isRectangleOverlap(int[] rec1, int[] rec2) {
-        // Check if there is positive overlap on X-axis
-        boolean overlapX = Math.max(rec1[0], rec2[0]) < Math.min(rec1[2], rec2[2]);
 
-        // Check if there is positive overlap on Y-axis
-        boolean overlapY = Math.max(rec1[1], rec2[1]) < Math.min(rec1[3], rec2[3]);
+        int leftEdge = Math.max(rec1[0], rec2[0]);
+        int rightEdge = Math.min(rec1[2], rec2[2]);
 
-        return overlapX && overlapY;
+        int bottomEdge = Math.max(rec1[1], rec2[1]);
+        int topEdge = Math.min(rec1[3], rec2[3]);
+
+        boolean hasWidthOverlap = leftEdge < rightEdge;
+        boolean hasHeightOverlap = bottomEdge < topEdge;
+
+        return hasWidthOverlap && hasHeightOverlap;
     }
 }
