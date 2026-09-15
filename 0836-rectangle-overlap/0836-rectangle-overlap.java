8a1d40ddd18506d4eed86bab6a27cc8bb146d0ea
Rahul Kushwaha <rahulkcoding2023@gmail.com>
2026-09-15 15:51:19 +0530

    Time: 0 ms (100%), Space: 43 MB (5.48%) - LeetHub

--- 0836-rectangle-overlap/0836-rectangle-overlap.java
+++ 0836-rectangle-overlap/0836-rectangle-overlap.java
@@ -1,15 +1,7 @@
 class Solution {
     public boolean isRectangleOverlap(int[] rec1, int[] rec2) {
-
-        int leftEdge = Math.max(rec1[0], rec2[0]);
-        int rightEdge = Math.min(rec1[2], rec2[2]);
-
-        int bottomEdge = Math.max(rec1[1], rec2[1]);
-        int topEdge = Math.min(rec1[3], rec2[3]);
-
-        boolean hasWidthOverlap = leftEdge < rightEdge;
-        boolean hasHeightOverlap = bottomEdge < topEdge;
-
-        return hasWidthOverlap && hasHeightOverlap;
+        int width = Math.min(rec1[2], rec2[2]) - Math.max(rec1[0], rec2[0]);
+        int height = Math.min(rec1[3], rec2[3]) - Math.max(rec1[1], rec2[1]);
+        return width > 0 && height > 0;
     }
 }
